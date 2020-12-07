bc3ea8de06dd9669e383adc7094981a3b8a6c37f
gopali24 <56757115+gopali24@users.noreply.github.com>
2020-12-07 12:16:12 +0530

    Add files via upload

---
+++ server/data/deweyCode.js
@@ -0,0 +1,6 @@
+let deweyList = [
+     { number: 0, path: "Computer science, information & general works" },
+     { number: 1, path: "Computer science, information & general works/Knowledge" },
+     { number: 2, path: "Computer science, information & general works/The book" },
+     { number: 3, path: "Data Science" }
+]    
